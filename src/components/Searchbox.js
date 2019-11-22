17787a2649d39dc73af0bc4778007582b70248a3
Payam <thisispayam@gmail.com>
2019-11-21 19:05:48 -0800

    refactored the states options

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -1,63 +1,17 @@
 import React from 'react';
 
 const SearchBox = ({ searchChange, stateChange})=>{
+	let states = ['AK','AL','AR','AZ','CA','CO','CT','DC','DE','FL',
+	'GA','HI','IA','ID','IL','KS','KY','LA','MA','MD','ME','MI','MN',
+	'MT','NC','ND','NE','NH','NJ','NM','NV','NY','OH','OK','OR','PA',
+	'RI','SC','SD','TN','TX','UT','VA','VT','WA','WI','WV','WY'];
 	return (
     <div>
 		    <input className="pa2 ba b--yellow bg-lightest-yellow" type='search' placeholder="hospital name..." onChange={searchChange} />
 			<div class="filters pa2">
 				<select name="filter" onChange={stateChange}>
 					<option value="">State</option>
-					<option value="AK">AK</option>
-					<option value="AL">AL</option>
-					<option value="AR">AR</option>
-					<option value="AZ">AZ</option>
-					<option value="CA">CA</option>
-					<option value="CO">CO</option>
-					<option value="CT">CT</option>
-					<option value="DC">DC</option>
-					<option value="DE">DE</option>
-					<option value="FL">FL</option>
-					<option value="GA">GA</option>
-					<option value="HI">HI</option>
-					<option value="IA">IA</option>
-					<option value="ID">ID</option>
-					<option value="IL">IL</option>
-					<option value="IN">IN</option>
-					<option value="KS">KS</option>
-					<option value="KY">KY</option>
-					<option value="LA">LA</option>
-					<option value="MA">MA</option>
-					<option value="MD">MD</option>
-					<option value="ME">ME</option>
-					<option value="MI">MI</option>
-					<option value="MN">MN</option>
-					<option value="MO">MO</option>
-					<option value="MS">MS</option>
-					<option value="MT">MT</option>
-					<option value="NC">NC</option>
-					<option value="ND">ND</option>
-					<option value="NE">NE</option>
-					<option value="NH">NH</option>
-					<option value="NJ">NJ</option>
-					<option value="NM">NM</option>
-					<option value="NV">NV</option>
-					<option value="NY">NY</option>
-					<option value="OH">OH</option>
-					<option value="OK">OK</option>
-					<option value="OR">OR</option>
-					<option value="PA">PA</option>
-					<option value="RI">RI</option>
-					<option value="SC">SC</option>
-					<option value="SD">SD</option>
-					<option value="TN">TN</option>
-					<option value="TX">TX</option>
-					<option value="UT">UT</option>
-					<option value="VA">VA</option>
-					<option value="VT">VT</option>
-					<option value="WA">WA</option>
-					<option value="WI">WI</option>
-					<option value="WV">WV</option>
-					<option value="WY">WY</option>
+					{states.map(state => <option value={state}>{state}</option>)}
 				</select>
 			</div>
     </div>
